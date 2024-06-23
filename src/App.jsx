8c32b72428ded9249d06022dde55904eea9eb2b5
ramaherdianto <ramadhanherdianto35@gmail.com>
2024-06-23 19:24:02 +0700

    feat: created header section and install axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,40 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
-        <div className='min-h-screen flex flex-col items-center justify-center'>
-            <div className='flex justify-between'>
-                <a href='https://vitejs.dev' target='_blank'>
-                    <img src={viteLogo} className='logo' alt='Vite logo' />
-                </a>
-                <a href='https://react.dev' target='_blank'>
-                    <img src={reactLogo} className='logo react' alt='React logo' />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className='card'>
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p className='text-2xl text-red-300'>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+        <div className='min-h-screen w-full flex flex-col items-center justify-center gap-10'>
+            <section>
+                <h1 className='text-5xl font-semibold text-slate-00'>Ram API Movies</h1>
+            </section>
+            <section className='mt-40'>
+                <div className='w-full'>
+                    <div className='relative px-3 flex justify-between items-center w-[500px] h-16 rounded-lg shadow-lg bg-white overflow-hidden'>
+                        <div className='flex items-center h-full w-12 text-gray-300'>
+                            <svg
+                                xmlns='http://www.w3.org/2000/svg'
+                                className='h-6 w-6'
+                                fill='none'
+                                viewBox='0 0 24 24'
+                                stroke='currentColor'
+                            >
+                                <path
+                                    strokeLinecap='round'
+                                    strokeLinejoin='round'
+                                    strokeWidth='2'
+                                    d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+                                />
+                            </svg>
+                        </div>
+
+                        <input
+                            className='peer h-full w-full outline-none text-sm text-gray-700 pr-2'
+                            type='text'
+                            id='search'
+                            placeholder='Search something..'
+                        />
+                    </div>
+                </div>
+            </section>
         </div>
     );
 }
